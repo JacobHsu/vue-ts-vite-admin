e0db98c17863b8a6802e9661390740b57882304b
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-16 17:52:50 +0800

    feat: App.vue AppProvider defineComponent

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <AppProvider>
+    <RouterView />
+  </AppProvider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { AppProvider } from "/@/components/Application";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+    AppProvider,
   },
 });
 </script>
