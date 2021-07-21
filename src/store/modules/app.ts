edfb96ae76a1e0ac10bd2a7408eef328198ca6b5
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-21 16:16:34 +0800

    fix: types declare tsconfig.json skipLibCheck

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,5 +1,17 @@
 import { defineStore } from 'pinia';
+import type { ProjectConfig } from '/#/config';
+import type { BeforeMiniState } from '/#/store';
+import { ThemeEnum } from '/@/enums/appEnum';
 
+interface AppState {
+  darkMode?: ThemeEnum;
+  // Page loading status
+  pageLoading: boolean;
+  // project config
+  projectConfig: ProjectConfig | null;
+  // When the window shrinks, remember some states, and restore these states when the window is restored
+  beforeMiniInfo: BeforeMiniState;
+}
 export const useAppStore = defineStore({
   id: 'app',
 });
