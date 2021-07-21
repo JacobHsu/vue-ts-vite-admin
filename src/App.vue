19ea478429fea3cec3235e3a040c30d5a49ea922
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-21 14:48:52 +0800

    feat: setupStore npm pinia

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
   <AppProvider>
     <RouterView />
   </AppProvider>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { AppProvider } from "/@/components/Application";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import { AppProvider } from '/@/components/Application';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     AppProvider,
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router, { setupRouter } from "/@/router";
-
-
+import { createApp } from 'vue';
+import App from './App.vue';
+import { setupRouter } from '/@/router';
+import { setupStore } from '/@/store';
 async function bootstrap() {
   const app = createApp(App);
+
+  // Configure store
+  setupStore(app);
 
   // Configure routing
   setupRouter(app);
 
-  app.mount("#app", true);
+  app.mount('#app', true);
 }
 
 void bootstrap();
