65025e36b67c9c21aa8f4381a975e78a7470e4e2
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-23 17:02:09 +0800

    feat: vite-plugin-html

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { router, setupRouter } from '/@/router';
-import { setupRouterGuard } from '/@/router/guard';
+// import { router, setupRouter } from '/@/router';
+// import { setupRouterGuard } from '/@/router/guard';
 import { setupStore } from '/@/store';
 import { setupI18n } from '/@/locales/setupI18n';
 
@@ -9,20 +9,20 @@ async function bootstrap() {
   const app = createApp(App);
 
   // Configure store
-  setupStore(app);
+  // setupStore(app);
 
   // Configure routing
-  setupRouter(app);
+  // setupRouter(app);
 
   // Multilingual configuration
   await setupI18n(app);
 
   // router-guard
-  setupRouterGuard(router);
+  // setupRouterGuard(router);
 
   // Mount when the route is ready
   // https://next.router.vuejs.org/api/#isready
-  await router.isReady();
+  // await router.isReady();
 
   app.mount('#app', true);
 }
